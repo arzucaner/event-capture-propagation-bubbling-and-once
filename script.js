d0e165121405441648e429a367a3827b677a0681
Arzu Caner <arzuguneycaner@gmail.com>
2022-12-17 01:33:50 +0000

    Create event capture, propagation and bubbling
    This is JavaScript30 Challenge. This project is for understanding important concepts associated with JavaScript such as event capture, propagation, bubbling and once.

---
+++ script.js
@@ -0,0 +1,22 @@
+const divs = document.querySelectorAll('div');
+
+function logText(e) {
+  //console.log(this.classList.value);
+  //e.stopPropagation(); //stop bubbling!
+  console.log(this);
+}
+
+//divs.forEach(div => div.addEventListener('click', logText));
+
+//divs.forEach(div => div.addEventListener('click', logText));
+
+divs.forEach(div => div.addEventListener('click', logText{
+  capture: false,
+  once: true
+}));
+
+button.addEventListener('click',() => {
+  console.log('Click!!!');
+},  {
+  once: true
+});
